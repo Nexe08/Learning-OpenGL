8e3d5245c100428da7a6dacb6034cec402a5f2ab
Nexe08 <rohitrautela1408@gmail.com>
2022-11-25 09:45:45 +0530

    creating shape and color with initializing glut.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
+#include "GL/glut.h"
 
-using namespace std;
-
-int main()
+void glInit()
 {
-    cout << "Hello world!" << endl;
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+}
+
+void Display()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glBegin(GL_QUAD_STRIP);
+        for (float i = -1.0; i <= 1.0; i+= 0.01)
+        {
+            glColor3f(i, 1.0, 1.0);
+            glVertex2f(i, 0.0);
+            glVertex2f(i, 0.5);
+        }
+    glEnd();
+
+    glFlush();
+}
+
+int main (int argc, char** argv)
+{
+    glutInit(&argc, argv); // init GLUT
+    glutCreateWindow("My window"); // name the window
+    glutInitWindowPosition(50, 50); // set window position
+    glutInitWindowSize(240, 240); // set window size
+    glutDisplayFunc(Display); // event handler for display
+    glutMainLoop(); // processing loop
     return 0;
 }
